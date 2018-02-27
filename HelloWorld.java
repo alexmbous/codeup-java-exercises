d6cf21873a91401f8348cc142b235288ac4b6b5a
alexander Bous <alexmbous@gmail.com>
2018-02-27 13:47:46 -0600

    First Commit and push on java exercises.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,50 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+
+        int myFavoriteNumber = 999;
+     System.out.println(myFavoriteNumber);
+
+        String myString = ("abcd");
+     System.out.print(myString);
+
+//    int myString = 3.14159;
+
+    float myNumber = 3.14f;
+    System.out.print(myNumber);
+
+
+        int i = 5;
+        System.out.println(i++);
+        System.out.println(i);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+//     String class = "1234";
+
+
+//        int x = 4;
+//        System.out.print(x+=5);
+
+//        int x = 3;
+//        int y = 4;
+//        System.out.print(y *= x);
+
+//        int x = 10;
+//        int y = 2;
+//        System.out.print(x /= y);
+//        System.out.print(y -= x);
+
+
+
+
+
+
+
     }
 }
 
